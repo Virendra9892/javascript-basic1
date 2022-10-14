b8534bc6fd094667e7e4b07d0d797ea6b87988d1
Virendra9892 <virendrap9892@gmail.com>
2022-10-14 17:58:17 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// function disp("a,b,c"){
+//     let d = "a+b+c";
+//      eval(d);
+// }
+
+// let sum = disp("4,2,7");
+// console.log("sum = ",sum);
+
+// let a = 20;
+// let b = 20;
+
+// let text = "a+b";
+
+// console.log(eval(text));
+
+// function sum(a,b,c){
+//     let c = 
+// }
+
+
+let a ="10";
+
+console.log(a[0]);
